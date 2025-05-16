3293a13986107e908dddc12a505c8be7bded07b5
schmidte10 <elliott.a.schmidt@gmail.com>
2025-05-16 12:19:29 +1000

    uploading population statistics module

--- Rscripts/PCAdapt_script2_bicolor.R
+++ Rscripts/PCAdapt_script2_bicolor.R
@@ -9,21 +9,29 @@ pdf("outputs/apoly_populations_kplot.pdf")
 plot(apoly_populations_kplot, option = "screeplot")
 dev.off() 
 
-apoly_populations_pca <- pcadapt(input =apoly_pcadapt, K =2)
+apoly_populations_pca <- pcadapt(input =apoly_pcadapt, K =3)
 write.table(apoly_populations_pca$scores, "outputs/pcadapt_scores.txt")
 summary(apoly_populations_pca)
 
 poplist.names <- read.delim("../data/population_list.txt", header =FALSE)
 
-pdf("outputs/apoly_populations_pca.pdf")
+pdf("outputs/apoly_populations_pca1v2.pdf")
 plot(apoly_populations_pca, option = "scores", i=1, j=2, pop = poplist.names$V3)
 dev.off()
 
-pdf("outputs/pcadapt_apoly_manhattan.pdf")
+pdf("outputs/apoly_populations_pca1v3.pdf")
+plot(apoly_populations_pca, option = "scores", i=1, j=3, pop = poplist.names$V3)
+dev.off()
+
+pdf("outputs/apoly_populations_pca2v3.pdf")
+plot(apoly_populations_pca, option = "scores", i=2, j=3, pop = poplist.names$V3)
+dev.off()
+
+png("outputs/pcadapt_apoly_manhattan.png")
 plot(apoly_populations_pca, option= "manhattan") 
 dev.off() 
 
-pdf("outputs/pcadapt_apoly_qqplot.pdf") 
+png("outputs/pcadapt_apoly_qqplot.png") 
 plot(apoly_populations_pca, option ="qqplot") 
 dev.off()
 
@@ -34,7 +42,7 @@ hist(apoly_populations_pca$pvalues, xlab ="p-values", main =NULL, breaks =50, co
 dev.off() 
 
 apoly_populations_padj <- p.adjust(apoly_populations_pca$pvalues, method ="bonferroni") 
-alpha <- 0.1 
+alpha <- 0.1 #[adjust as necessary]
 outliers <- which(apoly_populations_padj < alpha) 
 length(outliers) 
 
